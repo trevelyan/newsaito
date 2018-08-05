44f27736e8f289d6d631c32b3f9d1fdc8f1633ca
Bearguy <stephen@peterkins.ca>
2018-08-05 13:50:27 +0800

    more test coverage and continued JDoc

--- tests/blockchain.spec.js
+++ tests/blockchain.spec.js
@@ -1,5 +1,7 @@
 const assert = require('chai').assert
+const saito = require('../lib/saito')
 
+const Block = require('../lib/saito/block');
 const Blockchain = require('../lib/saito/blockchain');
 
 describe('BLOCKCHAIN', () => {
@@ -7,7 +9,26 @@ describe('BLOCKCHAIN', () => {
 
   describe('Constructor', () => {
     it('should have all necessary fields for a Blockchain object', () => {
-      assert(bchain.app !== undefined)
+      assert(bchain.app !== undefined);
+      assert.deepEqual(bchain.blocks, []);
+    });
+  });
+
+  describe('Return Latest Block', () => {
+    it('should return the latest Block from the Blockchain object', () => {
+      var newblock = new Block();
+      var tmpblock = bchain.returnLatestBlock();
+
+      tmpblock.block.unixtime = 0
+      newblock.block.unixtime = 0
+
+      assert.instanceOf(tmpblock, Block);
+      assert.deepEqual(tmpblock, newblock);
+
+      bchain.blocks.push(tmpblock)
+
+      var return_block = bchain.returnLatestBlock();
+      assert.deepEqual(tmpblock, return_block)
     });
   });
 
--- tests/mempool.spec.js
+++ tests/mempool.spec.js
@@ -15,5 +15,4 @@ describe('MEMPOOL', () => {
       assert.deepEqual(mempool.recovered, [])
     });
   });
-
 });
