2b3519322e253651ba0e200c9231ad6f641cdaf6
Stephen Peterkins <stephen@hideyoshi.local>
2018-09-05 18:54:29 -0400

    minor fixes to testing setup

--- tests/fork.spec.js
+++ tests/fork.spec.js
@@ -10,12 +10,12 @@ describe('FORK', () => {
   var app = {};
 
   const config = {
-    storage: { folder: "test/env", src: "test/fork_test_1" },
+    storage: { dest: "test/env", src: "test/fork_test_1" },
     setup: async () => {
-      await fs.remove(`../lib/data/${config.storage.folder}`);
+      await fs.remove(`../lib/data/${config.storage.dest}`);
       await fs.copy(
         `../lib/data/${config.storage.src}`, 
-        `../lib/data/${config.storage.folder}`, 
+        `../lib/data/${config.storage.dest}`, 
         { preserveTimestamps: true }
       );
     }
@@ -31,8 +31,12 @@ describe('FORK', () => {
 
   describe('should handle forked block files successfully', () => {
     it('Saves all available block files to blockchain', () => {
-      // console.log(app);
+      console.log(app.blockchain);
       assert.equal(app.blockchain.blocks.length, 9);
+    });
+
+    it('lc values should be total the amount of valid blocks on chain minus 1', () => {
+      assert.equal(app.blockchain.index.lc.reduce((tot, sum) => tot + sum), app.blockchain.index.bid.max() - 1);
     });
   });
 
